78b46af3d97881f530ccce0ea553e69af787e035
bonce-game <cema2838@gmail.com>
2024-10-23 21:24:01 +0300

    Миниигра

--- Assets/_Project/App/Code/Player.cs
+++ Assets/_Project/App/Code/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    Vector2 moveDir = Vector2.zero;
+    [SerializeField]
+    float speed = 5.0f;
+
+
+    private Vector2 posRb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        posRb = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleUpdate();
+        LogicUpdate();
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void HandleUpdate()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.y = Input.GetAxis("Vertical");
+    }
+    void LogicUpdate()
+    {
+        posRb.x = transform.position.x;
+        posRb.y = transform.position.y;
+    }
+    void Move()
+    {
+        rb.MovePosition(moveDir * speed * Time.deltaTime + posRb);
     }
 }
